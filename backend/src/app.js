3cb52ea9d972482da14f9e10bdf95b2460f514fa
Takku1N <lightroj00@gmail.com>
2025-10-10 16:46:28 +0700

    Test Jenkins

--- backend/src/app.js
+++ backend/src/app.js
@@ -46,7 +46,7 @@ app.use(express.json());
 app.use(cookieParser())
 //cors ต้องแก้ตอนลง production
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost");
+  res.setHeader('Access-Control-Allow-Origin', "http://35.186.152.229");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
